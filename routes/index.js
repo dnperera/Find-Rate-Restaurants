226b72d734e1d1543be916650329229242793148
Dayan Perera <dayannp@gmail.com>
2018-04-16 22:40:36 -0700

    setup layout with pug

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-	//res.send('Hey! It works!');
-	res.render('hello');
+	res.render('hello', { title: 'Find & Rate Resturants', city: 'San Rafael' });
 });
 
+router.get('/reverse/:name/:city', (req, res) => {
+	console.log([...req.params.city]);
+	//console.log([...req.params.name].reverse().join(''));
+	res.send('Name reversed !!!');
+});
 module.exports = router;
