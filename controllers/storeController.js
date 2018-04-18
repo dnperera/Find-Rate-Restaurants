2843283e437bf009624a28eff63e5e96c17077c7
Dayan Perera <dayannp@gmail.com>
2018-04-17 23:07:27 -0700

    complete add new store to DB

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
 	res.render('index');
 };
 
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add New Store' });
+};
+
+exports.createStore = async (req, res) => {
+	const store = new Store(req.body);
+	await store.save();
+	res.redirect('/');
 };
