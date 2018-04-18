22aea432925790ec5b591863ddf4763d404ea3a2
Dayan Perera <dayannp@gmail.com>
2018-04-18 11:35:28 -0700

    list avaliable stores, display flash error messages

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,7 +10,11 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
-	await store.save();
-	res.redirect('/');
+	const store = await new Store(req.body).save();
+	req.flash('success', `Successfully created ${store.name}.Care to leave a review ?.`);
+	res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = (req, res) => {
+	res.render('stores', { title: 'Stores' });
 };
