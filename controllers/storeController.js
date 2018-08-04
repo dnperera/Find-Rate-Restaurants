9f9a369746a43efec694fbb583c0529c96a7c7e5
Dayan Perera <dayannp@gmail.com>
2018-08-04 00:01:00 -0700

    add display all stores functionality

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -12,3 +12,9 @@ exports.createStore = async (req, res) => {
   req.flash("success", `Successfully new store ${store.name} was created`);
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  //Find all stores from the db
+  const stores = await Store.find();
+  res.render("stores", { title: "stores", stores: stores });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   homepage,
   addStore,
-  createStore
+  createStore,
+  getStores
 } = require("../controllers/storeController");
 const { catchErrors } = require("../handlers/errorHandlers");
 // Do work here
-router.get("/", homepage);
+router.get("/", catchErrors(getStores));
+router.get("/stores", catchErrors(getStores));
 router.get("/add", addStore);
 router.post("/add", catchErrors(createStore));
 
